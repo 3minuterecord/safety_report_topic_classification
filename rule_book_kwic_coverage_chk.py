90a36d1fe8d1540fb50148d1a40be66a5ef5b9d7
Jason Payne <j.payne.1976@gmail.com>
2022-08-29 11:09:28 +0100

    Create script for assessing coverage of rule-book

---
+++ rule_book_kwic_coverage_chk.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import rule_book_functs as rbfuncts
+from datetime import datetime
+import numpy as np
+
+# Description:
+# +--------------------------------------------------------------------+
+# 1. Load raw incident data, concatenate & remove unused fields
+# 2. Load rule book definitions
+# 3. Apply rule book based on 'kwic' & synonyms to classify text
+# 4. Save sample of classified texts for scoring
+# +--------------------------------------------------------------------+
+
+def main():     
+        # Create the sample without replacement
+        print('\n')
+        sample_size = int(input('Sample size: '))
+        num_experim = int(input('Number of tests: '))      
+        
+        # Current date and time for referencing filename
+        now = datetime.now() 
+        date_time = now.strftime("%y%m%d%H%M%S") 
+        
+        # Open a log file for results
+        log_file = open(f'coverage_checks/{date_time}_log.txt', 'w')  
+        for t in range(num_experim):  
+                # Load raw incident data
+                req_cols1 = ['IncidentNumber', 'ShortDescription', 'FullDescription', 'ImmediateAction']
+                req_cols2 = ['ID', 'EventTitle', 'Final Narrative']
+                req_cols3 = ['text']
+                incidents = pd.read_csv("data/source/20220413_D1_Incidents.csv", dtype=str, usecols=req_cols1)  
+                osha_incs = pd.read_csv("data/source/OSHA_January2015toJuly2021.csv", dtype=str, usecols=req_cols2)  
+                manu_fabr = pd.read_csv("data/source/manually_fabricated.csv", dtype=str, usecols=req_cols3)  
+                
+                # Load the 'kwic' rule definitions
+                # 'kwic' = Keyword in context
+                rul_csv = pd.read_csv('data/rule_book_kwic.csv')
+
+                # Some clean up for Wood dataset
+                # Concatenate some of the fields to make the 'text' field for searching
+                incidents.rename(columns={'IncidentNumber': 'incident_id'}, inplace=True)
+                incidents['text'] = (
+                        incidents['ShortDescription'].astype(str).fillna('') + ' ' + 
+                        incidents['FullDescription'].astype(str).fillna('') + ' ' + 
+                        incidents['ImmediateAction'].astype(str).fillna('')
+                ).str.lower()
+                incidents['dataset'] = 'ORGP'
+
+                # We only need the incident ID and the text for now
+                incidents = incidents[['incident_id', 'dataset', 'text']]
+
+                # Some clean up for OSHA dataset
+                # Concatenate some of the fields to make the 'text' field for searching
+                osha_incs.rename(columns={'ID': 'incident_id'}, inplace=True)
+                osha_incs['text'] = (
+                        osha_incs['EventTitle'].astype(str).fillna('') + ' ' + 
+                        osha_incs['Final Narrative'].astype(str).fillna('')
+                ).str.lower()
+                osha_incs['dataset'] = 'OSHA'
+
+                # We only need the incident ID and the text for now
+                osha_incs = osha_incs[['incident_id', 'dataset', 'text']]
+
+                # Add identifier fields to the manually fabricated dataset
+                manu_fabr['dataset'] = 'MANF'
+                manu_fabr['text'] = manu_fabr['text'].str.lower()
+                manu_fabr['incident_id'] = [i+1 for i in range(len(manu_fabr))]
+        
+                # Load the synonym database
+                syns_data = pd.read_csv('synonyms.csv')
+                syns_data['syn'] = syns_data['syn'].apply(rbfuncts.replace_syns)
+                              
+                # Combine all three (3) datasets
+                #df_all = pd.concat([incidents, osha_incs, manu_fabr])
+                df_all = pd.concat([incidents, osha_incs])
+                
+                # Randomly sample (proportionate sampling) from dataset
+                sample_frac = (sample_size/len(df_all))
+                
+                test_str = f'Test {t+1}'
+                print(f'\n{test_str}')
+                print('='*len(test_str))
+                sample = df_all.groupby('dataset', group_keys=False).apply(lambda x: x.sample(frac=sample_frac))
+                categories = rbfuncts.kwic_rule_book_scan(
+                        rules=rul_csv, 
+                        docs=sample["text"], syns_db=syns_data, run_rules='all', verb=False
+                )
+                
+                # Now tidy up the presentation of the output for printing
+                cats = []
+                for entry in categories:
+                        if ', '.join(entry) == '':
+                                cats.append('*** Not Classified') # So as to easily identify unclassified texts
+                        else:                
+                                cats.append(', '.join(entry))
+
+                # Convert to a simple datafrae
+                out_df = pd.DataFrame(cats, columns=['category'])
+                out_df['text'] = sample['text'].tolist()
+                out_df['dset'] = sample['dataset'].tolist()
+
+                unclassified_count = len(out_df.loc[(out_df.category == '*** Not Classified')])
+                check_count = len(out_df) - unclassified_count   
+                class_perct = round(100*check_count/sample_size)
+                print('Classified count: ', check_count)
+                print(f'Classified perct: {class_perct}%')
+                
+                # Write output in latex format for copy + paste into report (for tabulated results)
+                write_str = f'{t+1} & {date_time} & {sample_size} & {class_perct}\\'
+                log_file.write("%s\n" % write_str)     
+                
+                # current date and time
+                now = datetime.now() 
+                date_time = now.strftime("%y%m%d%H%M%S") 
+                
+                # Save the output file with classifications for verification/checking                                      
+                out_df.to_csv(f'coverage_checks/{date_time}_{sample_size}_{int(class_perct)}_sample_coverage.csv', index=False)                
+        
+        log_file.close()        
+        
+if __name__ == "__main__":
+        main()
---
+++ rule_book_kwic_run_on_file.py
@@ -0,0 +1,84 @@
+from unicodedata import category
+import pandas as pd
+import rule_book_functs as rbfuncts
+from tqdm.notebook import tqdm
+from io import StringIO
+
+# Description:
+# +--------------------------------------------------------------------+
+# 1. Load an incident file (small) or enter text of an incident directly
+# 2. Load rule book definitions
+# 3. Run rule book function based on 'kwic' & synonyms to classify text
+# +--------------------------------------------------------------------+
+
+def main():
+        
+        # Run from file or text:
+        run_type = input("Run from file ('f') or text ('t'): ")
+        
+        if run_type == 't':
+                text_in = StringIO(input("Paste text: "))
+                docs = pd.DataFrame(text_in, columns = ['text'])
+                docs["dset"] = 'ONFY'
+                docs_test=docs["text"]                
+        else:        
+                sample_ref = input("Enter file ref.: ")
+                docs = pd.read_csv(f"test_samples/{sample_ref}_100_sample_scores.csv", dtype=str)
+                docs_test=docs["text"]
+        
+        # 'kwic' = Keyword in context
+        rul_csv = pd.read_csv('data/rule_book_kwic.csv')
+        
+        # Load the synonym database
+        syns_data = pd.read_csv('synonyms.csv')
+        syns_data['syn'] = syns_data['syn'].apply(rbfuncts.replace_syns)
+
+        # Get categories
+        rule_extent = input("Run all rules (state: 'all') or selected rule (e.g., state: 'slips & trips'): ")
+        if rule_extent == 'd':
+                topic_groups = set(list(rul_csv.group))
+                for group in topic_groups:
+                        print('Checking: ', group)
+                        categories = rbfuncts.kwic_rule_book_scan(rules=rul_csv, docs=docs_test, syns_db=syns_data, run_rules=group)
+        else:        
+                categories = rbfuncts.kwic_rule_book_scan(rules=rul_csv, docs=docs_test, syns_db=syns_data, run_rules=rule_extent, verb=False)
+
+        # Now tidy up the presentation of the output for printing
+        # This will help with analysis/review of classifications and improvements to rule-book
+        cats = []
+        for entry in categories:
+                if ', '.join(entry) == '':
+                        cats.append('*** Not Classified') # So as to easily identify unclassified texts
+                else:                
+                        cats.append(', '.join(entry))
+
+        # Convert to a simple datafrae
+        out_df = pd.DataFrame(cats, columns=['category'])
+        out_df['text'] = docs['text'].tolist()
+        out_df['dset'] = docs['dset'].tolist()
+
+        unclassified_count = len(out_df.loc[(out_df.category == '*** Not Classified')])
+        classified_count = len(out_df) - unclassified_count
+        classified_count = "{:,}".format(classified_count)
+        classified_percent = round(100 - round(100*(unclassified_count / len(out_df)), 1), 1)
+
+        out_df.to_csv(f'output/{rule_extent}_{classified_count}_out_df_temp.csv')
+        
+        # Print to console in a review-friendly manner
+        print('\n')
+        for r in range(len(out_df)):
+                cat_title = f'{r+1}: {out_df.category[r]}'
+                print(cat_title)
+                print('='*len(cat_title))
+                print(out_df.text[r])
+                print('~ ' + out_df.dset[r])
+                print('\n')
+
+        final_str = f' Total classified: {classified_percent}% ({classified_count})'
+        print('+'+'-'*(len(final_str)-1)+'+')
+        print(final_str)
+        print('+'+'-'*(len(final_str)-1)+'+')
+        print('\n')
+        
+if __name__ == "__main__":
+        main()
